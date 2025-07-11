0496c3f8989beb3f0b2ed113a9391c2470b9f8f9
DamianoMura <mura.damiano14@gmail.com>
2025-07-11 19:30:05 +0200

    [upd] completed random extraction

--- js/script.js
+++ js/script.js
@@ -7,10 +7,19 @@ const simonSays = () => {
 
   //avviamo un ciclo perchè ci servono 5 numeri
   for (let i=0 ; i<5 ; i++){
-  console.log(`estrazione del numero in posizione ${i+1}...`) //debug
-  //nuovo numero estratto
-  let insertion = Math.floor(Math.random()*30 + 1);
-  console.log(`il numero estratto è ${insertion}...`)         //debug
+    console.log(`estrazione del numero in posizione ${i+1}...`)               //debug
+    //nuovo numero estratto
+    let insertion = Math.floor(Math.random()*30 + 1);
+    console.log(`il numero estratto è ${insertion}...`)                       //debug
+    if( isPresent(numbers,insertion)===true){
+      //se il numero è presente
+      numbers[i]=number;
+      console.log(`il numero ${insertion} è stato inserito nell'array...`)    //debug
+    }
+    else{
+      // se viene trovato semplicemente decrementiamo l'indice i per reinserire il numero
+    i--;
+    }
   }
 }
 //creiamo una funzione dove come parametri diamo un array di numeri  e un numero
